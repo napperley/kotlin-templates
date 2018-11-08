647855be08f5da00bcaf15698e137181e9cf537e
Nick Apperley <napperley@gmail.com>
2018-11-08 16:41:41 +1300

    Added initial implementation of server.

--- ktor/build.gradle.kts
+++ ktor/build.gradle.kts
@@ -1,2 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 group = "org.example"
 version = "0.1-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://dl.bintray.com/kotlin/ktor") }
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
+}
+
+plugins {
+    kotlin(module = "jvm") version "1.3.0"
+    application
+}
+
+application {
+    mainClassName = "org.example.ktor_server.MainKt"
+}
+
+dependencies {
+    val kotlinVer = "1.3.0"
+    val ktorVer = "1.0.0-beta-3"
+    val log4jVer = "1.7.25"
+    implementation(kotlin(module = "stdlib-jdk8", version = kotlinVer))
+    implementation("io.ktor:ktor-server-netty:$ktorVer")
+    implementation("org.slf4j:slf4j-log4j12:$log4jVer")
+}
+
+val run by tasks.getting {
+    doFirst { println("Starting server...") }
+    doLast { println("Server ready.") }
+}
+
+tasks.withType<KotlinCompile>().all {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ ktor/src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+@file:Suppress("PackageName")
+
+package org.example.ktor_server
+
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+    embeddedServer(factory = Netty, port = 8080) {
+        routing {
+            get("/") { call.respondText { "Hello World! :)" } }
+        }
+    }.start(wait = true)
+}
