7137c9a13b24163ca8e1e053ef03ea4e60ea98f0
Nick Apperley <napperley@gmail.com>
2018-11-09 15:27:25 +1300

    Added functionality to print contents of a web page.

--- fuel/build.gradle.kts
+++ fuel/build.gradle.kts
@@ -13,9 +13,19 @@ plugins {
 
 dependencies {
     val kotlinVer = "1.3.0"
+    val fuelVer = "1.16.0"
+    val coroutinesCoreVer = "1.0.0"
+
     implementation(kotlin(module = "stdlib-jdk8", version = kotlinVer))
+    implementation("com.github.kittinunf.fuel:fuel:$fuelVer")
+    implementation("com.github.kittinunf.fuel:fuel-coroutines:$fuelVer")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesCoreVer")
 }
 
 application {
     mainClassName = "org.example.fuel.MainKt"
+}
+
+val run by tasks.getting(JavaExec::class) {
+    args("https://www.google.com")
 }
--- fuel/src/main/kotlin/main.kt
+++ fuel/src/main/kotlin/main.kt
@@ -1,5 +1,15 @@
 package org.example.fuel
 
-fun main(args: Array<String>) {
-    TODO("Complete fuel template.")
+import awaitString
+import com.github.kittinunf.fuel.httpGet
+import kotlinx.coroutines.runBlocking
+
+fun main(args: Array<String>) = runBlocking {
+    println("Contents of ${args.first()}:\n${doHttpGet(args[0])}")
+    println("Exiting...")
+}
+
+private suspend fun doHttpGet(url: String): String {
+    val timeout = 5000
+    return url.httpGet().apply { timeout(timeout) }.awaitString()
 }
